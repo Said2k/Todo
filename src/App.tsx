c362d3ed176aaf18c7463bd82388ba6f2e3b646f
said Lansarov <saidlansarov12@gmail.com>
2023-02-06 19:05:34 +0300

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route,Routes} from 'react-router-dom'
+import TodosPage from './pages/TodosPage';
+import AboutPage from './pages/AboutPage';
+import OneTodo from './pages/OneTodo';
+import Completed from './pages/Completed';
+import Navbar from './components/Navbar';
 
-function App() {
+
+
+
+const App: React.FC = () => {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar/>
+    <div className='container'>
+    <Routes>
+      <Route element={<TodosPage/>} path='/' />
+      <Route element={<AboutPage/>} path='/about' />
+      <Route element={<OneTodo/>} path='/oneTodo/:id' />
+      <Route element={<Completed/>} path='/completed' />
+    </Routes>
     </div>
+    </>
   );
-}
+};
 
 export default App;
